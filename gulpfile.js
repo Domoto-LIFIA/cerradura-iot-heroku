385064d5f91ec9e06968f7ed26b940927dd6be9f
José Lobo <jloboprs@gmail.com>
2016-05-19 23:12:52 -0500

    Mejorar la administración de los recursos externos

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp'),
+  shell = require('gulp-shell'),
+  install = require('gulp-install'),
   nodemon = require('gulp-nodemon'),
-  plumber = require('gulp-plumber'),
   livereload = require('gulp-livereload');
 
+
+gulp.task('install', ['install-type'], function() {
+  return gulp.src(['./bower.json', './package.json']).pipe(install());
+});
+
+gulp.task('install-type', shell.task(['typings install']));
 
 gulp.task('develop', function () {
   livereload.listen();
